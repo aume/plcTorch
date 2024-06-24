9b30780299ab65d5b435147c2dec163a919e0279
Miles Thorogood <miles.thorogood@gmail.com>
2024-06-24 14:17:35 +1000

    added functionality to Dataset loader

--- PLCModel.py
+++ PLCModel.py
@@ -31,11 +31,42 @@ class AudioDataset(Dataset):
 
     def __getitem__(self, idx):
         audio, sample_rate = torchaudio.load(self.file_list[idx])
+        audio=audio.to(self.device)
+        # audio=self._resample(audio,sample_rate)   
+        # audio=self._mix_down(audio)
+        # audio=self._cut(audio)
+        # audio=self._right_pad(audio)
+
         audio = audio.squeeze(0)  # Ensure it's mono
         # Simulate packet loss
         mask = (torch.rand(audio.shape) > self.packet_loss_rate).float()
         corrupted_audio = audio * mask
         return corrupted_audio, audio, mask
+    
+    def _resample(self,waveform,sample_rate):
+        # used to handle sample rate
+        resampler=torchaudio.transforms.Resample(sample_rate,self.target_sample_rate)
+        return resampler(waveform)
+    
+    def _mix_down(self,waveform):
+        # used to handle channels
+        waveform=torch.mean(waveform,dim=0,keepdim=True)
+        return waveform
+    
+    def _cut(self,waveform):
+        # cuts the waveform if it has more than certain samples
+        if waveform.shape[1]>self.num_samples:
+            waveform=waveform[:,:self.num_samples]
+        return waveform
+    
+    def _right_pad(self,waveform):
+        # pads the waveform if it has less than certain samples
+        signal_length=waveform.shape[1]
+        if signal_length<self.num_samples:
+            num_padding=self.num_samples-signal_length
+            last_dim_padding=(0,num_padding) # first arg for left second for right padding. Make a list of tuples for multi dim
+            waveform=torch.nn.functional.pad(waveform,last_dim_padding)
+        return waveform
 
 def collate_fn(batch):
     corrupted_audios, original_audios, masks = zip(*batch)
